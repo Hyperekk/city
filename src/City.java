aa021bcfb16504ff9d0ef83a722e195ac815d203
Hyperekk <barttolaj@gmail.com>
2023-02-10 22:38:15 +0100

    razcej dziala ale nei wiem jak wyswietlic zawartosc listy

---
+++ src/City.java
@@ -0,0 +1,12 @@
+public class City
+{
+    public String nazwaMiasta;
+    public int liczbaMieszkancow;
+    public String powierzchniaMiasta;
+
+    public City(String nazwaMiasta, int liczbaMieszkancow, String powierzchniaMiasta) {
+        this.nazwaMiasta = nazwaMiasta;
+        this.liczbaMieszkancow = liczbaMieszkancow;
+        this.powierzchniaMiasta = powierzchniaMiasta;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        List<City> miasta = new ArrayList<>();
+
+        City będzin = new City("będzin", 10000, "5000km3");
+
+        miasta.add(będzin);
+
+        System.out.println(/*nie wiem jak to pokazac*/);
+    }
+}
